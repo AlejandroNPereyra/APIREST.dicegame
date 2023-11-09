3a20f25139b301c539f5a8784fbc1022bcba971e
AlejandroNPereyra <pereyra_alejandro@yahoo.com.ar>
2023-11-09 12:49:33 +0100

    + game number on json response

--- dicegame/app/Http/Controllers/api/GameController.php
+++ dicegame/app/Http/Controllers/api/GameController.php
@@ -26,10 +26,11 @@ class GameController extends Controller
         $games = Game::where('user_id', $id)
         
         ->get()
-        ->map(function ($games) {
+        ->map(function ($games, $index) {
 
             return [
 
+                'game_number' => $index +1,
                 'game_id' => $games->id,
                 'dice_A' => $games->dice_A,
                 'dice_B' => $games->dice_B,
@@ -41,6 +42,33 @@ class GameController extends Controller
 
         return response()->json(['Games Index for ' . $user->alias => $games]);
         
+    }
+
+    public function addGame ($id) {
+
+        // Find the user
+        $user = User::find($id);
+     
+        // Check if the user exists and has the 'gamer' role
+        if (!$user || !$user->hasRole('gamer')) {
+            return response()->json(['message' => 'User not found or not a gamer'], 404);
+        }
+     
+        // Generate two random dice rolls
+        $dice1 = rand(1, 6);
+        $dice2 = rand(1, 6);
+     
+        // Create a new game with these dice rolls
+        $game = Game::create([
+
+            'user_id' => $id,
+            'dice_A' => $dice1,
+            'dice_B' => $dice2,
+
+        ]);
+     
+        return response()->json(['message' => 'Game added successfully', 'game' => $game]);
+
     }
 
 }
